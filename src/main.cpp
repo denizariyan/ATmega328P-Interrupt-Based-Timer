0e81e3fdf2e372930becfec9d743d9c17410771d
Deniz Ariyan <denizariyan@gmail.com>
2022-06-05 15:35:08 +0300

    Improve code comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,20 @@
 #include <avr/interrupt.h>
 
 ISR (TIMER1_OVF_vect) {
-    PORTB ^= (1 << PORTB5);
-    TCNT1 = 64755; // 50 ms
+    PORTB ^= (1 << PORTB5); // Toggle the 5th data register of PORTB
+    TCNT1 = 64755; // 50 ms for 16MHz clock
 }
 
-int main(void) {
-    DDRB = (0x01 << DDB5);
-    TCNT1 = 64755; // 50 ms
-    TCCR1A = 0x00;
-    TCCR1B = (1<<CS10) | (1<<CS12);
-    TIMSK1 = (1 << TOIE1);
-    sei();
+int main() {
+    DDRB = (1 << DDB5); // Set 5th data direction register of PORTB. A set value means output
+    TCNT1 = 64755; // 50 ms for 16MHz clock
+    TCCR1A = 0x00; // Set normal counter mode
+    TCCR1B = (1<<CS10) | (1<<CS12); // Set 1024 pre-scaler
+    TIMSK1 = (1 << TOIE1); // Set overflow interrupt enable bit
+    sei(); // Enable interrupts globally
     while(1)
     {
-        // Do anything, this timer in non-blocking
+        // Do anything, this timer is non-blocking. It will interrupt the CPU only when needed
     }
     // Add return so old compilers don't cry about it being missing. Under normal circumstances this will never be hit
     return 0;
